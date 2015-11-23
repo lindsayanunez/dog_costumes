d27c4d225de3431b726af7d4cc7e2ec6b6e04bb3
Lindsay Nunez <lindsayanunez@gmail.com>
2015-11-23 09:24:10 -0600

    Created cart

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,5 +1,11 @@
 class StorefrontController < ApplicationController
-  def index
-    @products = Product.all
+    def index
+      if params[:cat_id]
+        @category = Category.find(params[:cat_id])
+        @products = Product.where(category_id: @category.id)
+
+      else
+      @products = Product.all
+    end
   end
 end
